32c1827635d94fe52b2ac75dc77d473177ff8986
KINGSABRI <king.sabri@gmail.com>
2013-09-15 22:40:01 +0300

    Fix require commands

--- lib/formrude/ui/console/shell.rb
+++ lib/formrude/ui/console/shell.rb
@@ -1,8 +1,8 @@
 begin
+  require 'formrude/ui/console/shell/commands'
   require 'formrude/ui/console/shell/shell_core'
   require 'formrude/ui/console/shell/shell_config'
   require 'formrude/ui/console/shell/history'
-  require 'formrude/ui/console/shell/commands'
 rescue Exception => e
   puts "\n[!] Error from File: #{__FILE__} : #{e}\n\n"
 end
